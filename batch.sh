ebfab2556e4ccc4ca2148131a8f3e96c7d772d63
Trey Wenger <twenger2@wisc.edu>
2023-06-23 13:29:40 -0500

    update batch script

--- batch.sh
+++ batch.sh
@@ -3,16 +3,32 @@
 #SBATCH --job-name="g049_amoeba2"
 #SBATCH --output="logs/%x.%j.%N.out"
 #SBATCH --error="logs/%x.%j.%N.err"
-#SBATCH --nodes=1-20
-#SBATCH --ntasks-per-node=16
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=twenger2@wisc.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --export=ALL
-#SBATCH --time 2-00:00:00
-#SBATCH --array 0-890
+#SBATCH --time 00:10:00
+#SBATCH --array 1-45
 
-# N.B. Update --array above to match number of pixels to be fit
+# there are 890 pixels to fit
+NUM_JOBS=890
+
+# each "task" will fit 20 pixels
+PER_TASK=20
+
+# so there must be at least 45 tasks (--array)
+
+START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * $PER_TASK + 1 ))
+END_NUM=$(( $SLURM_ARRAY_TASK_ID * $PER_TASK ))
+END_NUM=$(( $END_NUM < $NUM_JOBS ? $END_NUM : $NUM_JOBS ))
+echo Task $SLURM_ARRAY_TASK_ID running $START_NUM to $END_NUM
 
 eval "$(conda shell.bash hook)"
 conda activate amoeba2
 
-python fit_G049_slurm.py $SLURM_ARRAY_TASK_ID
+for (( i=$START_NUM; i<=END_NUM; i++ )); do
+    python fit_G049_slurm.py $i
+done
+
